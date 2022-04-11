4ece40794656a3b755317cdbf326d7da97ee3b79
Mostafa772 <47507394+Mostafa772@users.noreply.github.com>
2022-04-11 15:00:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,83 +1,117 @@
-from abc import ABC, abstractmethod  
+from abc import ABC, abstractmethod
 from itertools import chain, combinations
+import random
 
-# Class Knapsack will take as input first the capacity of the knapsack 
-# then the number of the items K and finally the value of each item 
+
+# Class Knapsack will take as input first the capacity of the knapsack
+# then the number of the items K and finally the value of each item
 
 # a function to get all subsets of a set
 def powerset(iterable):
     s = list(iterable)
-    return chain.from_iterable(combinations(s, r) for r in range(len(s)+1))
+    return chain.from_iterable(combinations(s, r) for r in range(len(s) + 1))
+
+
+# Getting input from the user and if wrong we let him enter again
+def get_input():
+    while True:
+        try:
+            value = int(input())
+            break
+        except ValueError:
+            print("knapsack only accepts integer values please try again")
+
+    return value
+
 
 class Knapsack:
 
-  def __init__(self, init_max_items, init_max_weight):
-    self.items_prophet = []
-    self.items_weight = []
-    self.max_weight = init_max_weight
-    self.max_items = init_max_items
-    
-    print("please enter the weight for each of the " + str(self.max_items) + "th item")
-    for i in range(self.max_items):
-      self.items_weight.append(int(input()))
+    def __init__(self, init_max_items, init_max_weight):
+        self.items_profit = []
+        self.items_weight = []
+        self.max_weight = init_max_weight
+        self.max_items = init_max_items
 
-    print("please enter the prophet for each of the " + str(self.max_items) + "th item")
-    for i in range(self.max_items):
-      self.items_prophet.append(int(input()))
+        print("please enter the weight for each of the " + str(self.max_items) + "th item")
+        for i in range(self.max_items):
+            self.items_weight.append(get_input())
 
-  def __str__(self):
-    return f"""The number of items we have is: {str(self.max_items)} and the max_weight of the knapsack is: {str(self.max_weight)} 
-and the items have the weigths of {self.items_weight} and prophet of {self.items_prophet}"""  
-  
-  @abstractmethod
-  def solution(self):
-    pass
+        print("please enter the profit for each of the " + str(self.max_items) + "th item")
+        for i in range(self.max_items):
+            self.items_profit.append(get_input())
+
+    def __str__(self):
+        return f"""The number of items we have is: {str(self.max_items)} and the max_weight of the knapsack is: {str(self.max_weight)} 
+and the items have the weights of {self.items_weight} and profit of {self.items_profit}"""
+
+    @abstractmethod
+    def solution(self):
+        pass
 
 
 class BruteForce(Knapsack):
 
-  def solution(self):
-    max_amount = 0
+    def solution(self):
+        max_amount = 0
 
-    for p, w in zip(powerset(self.items_prophet), powerset(self.items_weight)):
-      #print(p, " || ",w)
-      if sum(w) > self.max_weight or len(w) > self.max_items:
-        continue
-      elif sum(p) > max_amount:
-        max_amount = sum(p)
+        for p, w in zip(powerset(self.items_profit), powerset(self.items_weight)):
+            if sum(w) > self.max_weight or len(w) > self.max_items:
+                continue
+            elif sum(p) > max_amount:
+                max_amount = sum(p)
 
-    return max_amount
-      #  print(len(choice), choice) 
+        return max_amount
 
-
-# first_test = Knapsack(1, 2)
-# print(first_test)
 
 class DynamicProgramming(Knapsack):
-  def solution(self):
-    
-    table = [[0 for x in range(self.max_weight + 1)] for y in range(self.max_items + 1)]
-    # print(table)
-    i = 0
-    while i < self.max_items + 1:
-      j = 0
-      while j < self.max_weight + 1:
-        if i == 0 or j == 0:
-          table[i][j] = 0
-        elif self.items_weight[i - 1] <= j:
-          table[i][j] = max(table[i - 1][j], table[i - 1][j - self.items_weight[i - 1]] + self.items_prophet[i - 1]) 
-        else:
-          table[i][j] = table[i - 1][j]
-        
-        j += 1
-      i += 1
-    # print(table)
-    return(table[self.max_items][self.max_weight])
+
+    def solution(self):
+        table = [[0 for x in range(self.max_weight + 1)] for y in range(self.max_items + 1)]
+        i = 0
+
+        while i < self.max_items + 1:
+            j = 0
+
+            while j < self.max_weight + 1:
+
+                if i == 0 or j == 0:
+                    table[i][j] = 0
+
+                elif self.items_weight[i - 1] <= j:
+                    table[i][j] = max(table[i - 1][j],
+                                      table[i - 1][j - self.items_weight[i - 1]] + self.items_profit[i - 1])
+
+                else:
+                    table[i][j] = table[i - 1][j]
+
+                j += 1
+            i += 1
+        return table[self.max_items][self.max_weight]
 
 
+# Greedy solution for fractional Knapsack problem
+# It will give optimal solution here but in case of
+# 0/1 knapsack problem it won't always give the
+# optimal solution
+class GreedySolution(Knapsack):
 
-# example = BruteForce(4, 8)
-example_two = DynamicProgramming(4, 8)
-print("we are here: " + str(example_two.solution())) 
-#print("we are here: " + str(example.solution())) 
-print(example_two)
+    def solution(self):
+        weight_to_profit = [(p / w) for w, p in zip(self.items_weight, self.items_profit)]
+        sum_of_weight = 0
+        max_profit = 0
+
+        while sum_of_weight < self.max_weight:
+            index = weight_to_profit.index(max(weight_to_profit))
+
+            if (sum_of_weight + self.items_weight[index]) <= self.max_weight:
+                sum_of_weight += self.items_weight[index]
+                max_profit += self.items_profit[index]
+                weight_to_profit[index] = -1
+
+            else:
+                ratio = ((self.max_weight - sum_of_weight) / self.items_weight[index])
+                sum_of_weight += ratio * self.items_weight[index]
+                max_profit += ratio * self.items_profit[index]
+                weight_to_profit[index] = -1
+
+        return max_profit
